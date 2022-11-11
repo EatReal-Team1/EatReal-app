120d2378ac0660d00ef8ce85c64b9d5b891f7066
Emily Feng <manqingf@andrew.cmu.edu>
2022-11-11 01:34:40 -0500

    review form replaced placeholder post image with image overlay function

--- EatReall-App/Views/Reviewing/FormView.swift
+++ EatReall-App/Views/Reviewing/FormView.swift
@@ -22,7 +22,7 @@ struct FormView: View {
   @State private var dishes = ""
   @State private var review = ""
   @State private var displayed = false
-  @State private var rating:Int = 4
+  @State private var rating:Int = 0
   
   @State private var submitted = false
   @State private var exit = false
@@ -50,7 +50,7 @@ struct FormView: View {
             Text("X")
               .padding(.horizontal, 30)
               .font(.system(
-                size: 35,
+                size: 25,
                 weight: .bold,
                 design: .default))
               .foregroundColor(.white)
@@ -61,9 +61,33 @@ struct FormView: View {
         }
         
         VStack {
-          Image("placeholder-post")
-            .resizable()
-            .frame(width: 150.0, height: 200.0)
+          ZStack{
+            Image("placeholder-food")
+              .resizable()
+              .aspectRatio(contentMode: .fill)
+              .frame(
+                width: 187,
+                height: 220)
+              .overlay(
+                Section {
+                  Image("placeholder-reaction")
+                    .resizable()
+                    .frame(width: 56,
+                           height: 56)
+                    .foregroundColor(.primary)
+                    .padding(8)
+                    .background(Color.gray)
+                    .clipShape(Circle())
+                    .background(
+                      Circle()
+                        .stroke(Color.black, lineWidth: 2)
+                    )
+                }.offset(x: -50, y: -80)
+              )
+          }
+          
+          Spacer().frame(height:20)
+
           
           Text("Providence, RI")
             .padding(.horizontal, 30)
@@ -74,12 +98,9 @@ struct FormView: View {
             .foregroundColor(.white)
         }
         
-        Spacer()
-
+        Spacer().frame(height:10)
         
         TextReview()
-        
-        Spacer()
         
         RatingReview(rating: $rating)
         
@@ -89,7 +110,7 @@ struct FormView: View {
           Text("Share")
             .padding(.horizontal, 30)
             .font(.system(
-              size: 35,
+              size: 25,
               weight: .bold,
               design: .default))
             .foregroundColor(.white)
