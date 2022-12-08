5a7a9ff0123f902d9d0ea409835e7406838df835
Zeke-Rong <zer@andrew.cmu.edu>
2022-12-08 03:34:04 -0500

    create views for memory and saved posts

--- EatReal/Views/Profile/MemoryView.swift
+++ EatReal/Views/Profile/MemoryView.swift
@@ -8,23 +8,17 @@
 import SwiftUI
 
 struct MemoryView: View {
+    @StateObject var viewRouter: ViewRouter
     var body: some View {
-        Spacer()
-        Button(action: {
         
-        }) {
-            Text("Memories").bold()
-                .font(Font.custom("Helvetica Neue", size: 25.0))
-                .padding(15)
-                .foregroundColor(Color.white)
-                .background(Color.black)
-                .cornerRadius(5)
-        }
+       Text("show all previous posts here")
+        Spacer()
+        navBar(viewRouter: viewRouter)
     }
 }
 
-struct MemoryView_Previews: PreviewProvider {
-    static var previews: some View {
-        MemoryView()
-    }
-}
+//struct MemoryView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MemoryView()
+//    }
+//}
