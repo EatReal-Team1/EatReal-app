dad0cc19ad71a54e8f288782f142cb615e877057
Zeke-Rong <zer@andrew.cmu.edu>
2022-12-14 15:12:04 -0500

    navBar works

--- EatReal/Views/Navigation/profileButton.swift
+++ EatReal/Views/Navigation/profileButton.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct profileButton: View {
-    @State var buttonClicked : String = "home"
+  //  @State var buttonClicked : String = "home"
     @ObservedObject var viewRouter: ViewRouter
     var body: some View {
         Button(action: {
-            self.buttonClicked = "profile";
+            navBar.buttonClicked = "profile";
           viewRouter.currentPage = .profile
         }) {
-            Image(self.buttonClicked == "profile" ? "profile-filled" : "profile-outline").resizable()
+            Image(navBar.buttonClicked == "profile" ? "profile-filled" : "profile-outline").resizable()
                 .frame(width: 40.0, height: 40.0)
         }
     }
